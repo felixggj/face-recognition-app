29cf31203a0247ae36c3b35222c602e296d50ddc
felixggj <me@felix-gomez.com>
2023-07-06 18:00:56 +0200

    Added images to storage database

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,6 +2,17 @@ import os
 import cv2
 import face_recognition
 import pickle
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://realtimefacerecognition-cf9b7-default-rtdb.europe-west1.firebasedatabase.app/',
+    'storageBucket': 'realtimefacerecognition-cf9b7.appspot.com'
+})
+
 
 # Import student images
 folderPath = 'images'
@@ -12,6 +23,13 @@ studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0]) # separates ending format from path in order to keep only the name
+
+    # Create a folder called images in the storage bucket inside firebase and upload all images there
+    fileName = os.path.join(folderPath, path) 
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
     # print(os.path.splitext(path))
     # print(os.path.splitext(path))
 print(studentIds)
